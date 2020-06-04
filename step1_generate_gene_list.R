cd4ddcbacb5408aee66497a97e17a95dff2c300b
cmutd <16281302+cmutd@users.noreply.github.com>
2020-06-03 19:36:39 -0500

    Update step1_generate_gene_list.R

--- step1_generate_gene_list.R
+++ step1_generate_gene_list.R
@@ -2,7 +2,6 @@ rm(list=ls())
 
 library(rvest) # Easily Harvest (Scrape) Web Pages
 library(RCurl) # General Network (HTTP/FTP/...) Client Interface for R
-library(clusterProfiler) # statistical analysis and visualization of functional profiles for genes and gene clusters
 
 options(stringsAsFactors = F)
 
@@ -10,7 +9,5 @@ options(stringsAsFactors = F)
 # The second table contains tumor suppressor genes 
 Suppressor_table<- html_table(read_html("https://www.cancerquest.org/cancer-biology/cancer-genes"),header=T)[[2]]
 
-genes <- Suppressor_table$`Tumor Suppressor`[c(1,3,4,7:10,12,14:16)]
-genes <- bitr(genes, fromType="SYMBOL", toType="ENTREZID", OrgDb="org.Hs.eg.db")
-genes$url <- paste0("https://www.ncbi.nlm.nih.gov/gene/",genes$ENTREZID)
+genes <- data.frame(SYMBOL=Suppressor_table[c(1,3,4,7:10,12,14:16),"Tumor Suppressor"])
 write.table(genes,"gene_list.txt",row.names = F,quote=F,sep='\t')
